0667de9b9c7fa3c9213ad35f5868b441f6607bc4
xs1523c <xs1523c@gmail.com>
2018-05-17 14:08:24 +0800

    建立後台的權限驗證

--- app/controllers/admin/travelresorts_controller.rb
+++ app/controllers/admin/travelresorts_controller.rb
@@ -1,7 +1,20 @@
 class Admin::TravelresortsController < ApplicationController
+  before_action :authenticate_admin
 
-def index
-  
-end
+  def index
+    
+  end
+
+
+
+
+  private
+
+  def authenticate_admin
+    unless current_user.admin?
+      flash[:alert]="Not Allow"
+      redirect_to root_path
+    end
+  end
 
 end
